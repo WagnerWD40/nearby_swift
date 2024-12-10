d33fc592c8dad5699d77cfb4dbe31c2470891397
Wagner Lemos Ferreira da Luz <wagner.luz@softplan.com.br>
2024-12-10 17:11:08 -0300

    Initial commit - project setup

---
+++ Nearby/FlowCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  FlorCoordinator.swift
+//  Nearby
+//
+//  Created by Wagner Lemos Ferreira da Luz on 10/12/24.
+//
+
+import Foundation
+import UIKit
+
+class FlowCoordinator {
+    private var navigationController: UINavigationController?
+    
+    public init() {
+        
+    }
+    
+    func start() -> UINavigationController? {
+        let startViewController = UIViewController()
+        startViewController.view.backgroundColor = .red
+        
+        self.navigationController = UINavigationController(rootViewController: startViewController)
+        
+        return navigationController
+    }
+    
+}
