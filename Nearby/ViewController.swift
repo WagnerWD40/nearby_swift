9214563dad40fdc89e92c56275bfc25ae5300f14
Wagner Lemos Ferreira da Luz <wagner.luz@softplan.com.br>
2024-12-10 16:49:37 -0300

    Initial Commit

---
+++ Nearby/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nearby
+//
+//  Created by Wagner Lemos Ferreira da Luz on 10/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
